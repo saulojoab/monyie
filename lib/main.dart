4a92f7d43b9b72c359af201b22160ffecc7ce136
Saulo Joab <saulojoabps@gmail.com>
2019-11-16 05:26:32 -0300

    simple login screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
-void main() => runApp(MyApp());
+void main() => {
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]).then((_){
+      runApp(new MyApp());
+    })
+};
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
@@ -9,24 +14,15 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.green,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: Login(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
+class Login extends StatefulWidget {
+  Login({Key key, this.title}) : super(key: key);
 
   // This widget is the home page of your application. It is stateful, meaning
   // that it has a State object (defined below) that contains fields that affect
@@ -40,20 +36,15 @@ class MyHomePage extends StatefulWidget {
   final String title;
 
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _LoginState createState() => _LoginState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _LoginState extends State<Login> {
+  String _email = "";
 
-  void _incrementCounter() {
+  void _setEmail(value){
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      _email: value;
     });
   }
 
@@ -66,46 +57,85 @@ class _MyHomePageState extends State<MyHomePage> {
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
+      body: Container(
+        color: Color.fromRGBO(0, 190, 130, 1),
+        child: Center(
         // Center is a layout widget. It takes a single child and positions it
         // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+            Container(
+              child: Padding(
+                padding: EdgeInsets.all(50.0, ),
+                child: 
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                  Icon(Icons.attach_money, size: 40, color: Color.fromRGBO(0, 150, 0, 1),),
+                  Text(
+                    'MONYIE',
+                    style: TextStyle(fontSize: 40, color: Color.fromRGBO(0, 150, 0, 1)),
+                  ),
+                ],)
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
+            Container(
+              width: 320,
+              child: Padding(
+                padding: EdgeInsets.all(10.0),
+                child: TextField(
+                  decoration: InputDecoration(
+                    hintText: 'E-mail',
+                    helperText: "Insira o seu e-mail acima",
+                    helperStyle: TextStyle(color: Color.fromRGBO(0, 120, 0, 1)),
+                    prefixIcon: Icon(Icons.email),
+                    hintStyle: TextStyle(color: Color.fromRGBO(0, 120, 0, 1)),
+                    //border: 
+                  ),
+                  style: TextStyle(color: Color.fromRGBO(0, 120, 0, 1)),
+                  onChanged: (text) => _setEmail(text),
+                  keyboardType: TextInputType.emailAddress,
+                ),
+              )
             ),
+            Container(
+              width: 320,
+              child: Padding(
+                padding: EdgeInsets.all(10.0),
+                child: TextField(
+                  decoration: InputDecoration(
+                    hintText: 'Senha',
+                    helperText: "Insira a sua senha acima",
+                    helperStyle: TextStyle(color: Color.fromRGBO(0, 120, 0, 1)),
+                    prefixIcon: Icon(Icons.lock),
+                    hintStyle: TextStyle(color: Color.fromRGBO(0, 120, 0, 1)),
+                    //border: 
+                  ),
+                  
+                  style: TextStyle(color: Color.fromRGBO(0, 120, 0, 1)),
+                  onChanged: (text) => _setEmail(text),
+                  obscureText: true,
+                ),
+              )
+            ),
+            Container(
+              child: 
+              Padding(
+                padding: EdgeInsets.all(20.0),
+                child:  FlatButton(
+                  autofocus: false,
+                  onPressed: () => print('pressed'),
+                  child: Text('Entrar', style: TextStyle(color: Colors.white),),
+                  color: Color.fromRGBO(0, 170, 100, 1),
+                ),
+              )
+             
+            )
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      )
     );
   }
 }
